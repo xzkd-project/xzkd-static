5ce7bbf7e0369a9789c055a7121829918a845f25
TianKai Ma <tiankaima@163.com>
2024-01-04 14:27:20 +0800

    feat: enable query by course_id

--- curriculum.py
+++ curriculum.py
@@ -12,17 +12,18 @@ from utils.jw import update_lectures
 from utils.tools import save_json
 
 
-async def fetch_course_info(session: aiohttp.ClientSession, semester_path: str, _courses: [Course], sem, progress_bar):
+async def fetch_course_info(session: aiohttp.ClientSession, semester_path: str, _courses: [Course], sem, progress_bar, course_api_path: str):
     async with sem:
         _courses = await update_lectures(session, _courses)
 
         for _course in _courses:
             save_json(_course, os.path.join(semester_path, f"{_course.id}.json"))
+            save_json(_course, os.path.join(course_api_path, f"{_course.id}"))
 
         progress_bar.update(len(_courses))
 
 
-async def fetch_semester(session: aiohttp.ClientSession, curriculum_path: str, semester_id: str):
+async def fetch_semester(session: aiohttp.ClientSession, curriculum_path: str, semester_id: str, course_api_path: str):
     # create "$(base_dir)/build/curriculum/$(semester_id)" directory if not exists
     semester_path = os.path.join(curriculum_path, semester_id)
     if not os.path.exists(semester_path):
@@ -39,7 +40,7 @@ async def fetch_semester(session: aiohttp.ClientSession, curriculum_path: str, s
 
     # split into chunks of 50 courses, and fetch them concurrently
     course_chunks = [courses[i:i + 50] for i in range(0, len(courses), 50)]
-    tasks = [fetch_course_info(session, semester_path, _courses, sem, progress_bar) for _courses in course_chunks]
+    tasks = [fetch_course_info(session, semester_path, _courses, sem, progress_bar, course_api_path) for _courses in course_chunks]
 
     with progress_bar:
         await asyncio.gather(*tasks)
@@ -48,8 +49,12 @@ async def fetch_semester(session: aiohttp.ClientSession, curriculum_path: str, s
 async def make_curriculum():
     base_path = os.path.dirname(os.path.abspath(__file__))
     curriculum_path = os.path.join(base_path, "build", "curriculum")
+    course_api_path = os.path.join(base_path, "build", "api", "course")
     if not os.path.exists(curriculum_path):
         os.mkdir(curriculum_path)
+
+    if not os.path.exists(course_api_path):
+        os.makedirs(course_api_path)
 
     async with aiohttp.ClientSession() as session:
         await jw_login(session)
@@ -58,7 +63,7 @@ async def make_curriculum():
         save_json(semesters, os.path.join(curriculum_path, "semesters.json"))
 
         for semester in tqdm(semesters):
-            await fetch_semester(session, curriculum_path, str(semester.id))
+            await fetch_semester(session, curriculum_path, str(semester.id), course_api_path)
 
 
 def main():
